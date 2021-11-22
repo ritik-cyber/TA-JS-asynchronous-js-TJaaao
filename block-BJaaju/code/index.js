a83be7201dc2bd89187ce6c2e164e092ca5fb43b
ritik-cyber <ritikdhingra03@gmail.com>
2021-11-22 23:18:40 +0530

    proper done

--- block-BJaaju/code/index.js
+++ block-BJaaju/code/index.js
@@ -1,33 +1,43 @@
-// let input = document.querySelector("input");
-// function handleSumbit(event) {
-//   if (event.keyCode === 13 && input.value) {
-//     let value = input.value;
-//     console.log(value);
-//   }
-// }
-
-// input.addEventListener("keydown", handleSumbit);
-const url =
-  "https://api.unsplash.com/photos/?client_id=3vt4Pgiypd6m7iDN4KmaZlQq5uwngLrT1S3uRy-tRPE";
-
+let searchElm = document.querySelector("input");
 let root = document.querySelector(".root");
 
+const searchUrl = (query) =>
+  `https://api.unsplash.com/search/photos?query=${query}&client_id=VayJM5gFpgw4UsvS8wyddd97zhzIZUyAZ4ptRCGNQZw`;
+
+const url =
+  "https://api.unsplash.com/photos/?client_id=VayJM5gFpgw4UsvS8wyddd97zhzIZUyAZ4ptRCGNQZw";
+
 function fetch(url, successHandler) {
+  console.log(url, successHandler);
   let xhr = new XMLHttpRequest();
   xhr.open("GET", url);
   xhr.onload = () => successHandler(JSON.parse(xhr.response));
+  // console.log(successHandler);
   xhr.onerror = function () {
     console.log("somehting went wrong");
   };
   xhr.send();
 }
 
-fetch(url, function (images) {
+function displayImage(images) {
+  root.innerHTML = "";
   images.forEach((image) => {
-    let li = document.createElement(li);
-    let img = document.createElement(img);
+    let li = document.createElement("li");
+    let img = document.createElement("img");
     img.src = image.urls.small;
     li.append(img);
     root.append(li);
   });
-});
+}
+
+fetch(url, displayImage);
+
+function handleSearch(event) {
+  if (event.keyCode === 13 && searchElm.value) {
+    fetch(searchUrl(searchElm.value), (searchResult) => {
+      displayImage(searchResult.results);
+    });
+  }
+}
+
+searchElm.addEventListener("keyup", handleSearch);
